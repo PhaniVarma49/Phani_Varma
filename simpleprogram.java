d8042ec0a32b1c23061164eea635d4ba977e929c
PhaniVarma49 <149029838+PhaniVarma49@users.noreply.github.com>
2026-03-24 11:03:38 +0530

    Update simpleprogram.java

--- simpleprogram.java
+++ simpleprogram.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class simpleprogram {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, Jenkins!");
